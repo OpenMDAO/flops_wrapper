a64d1799de39104761cdac11dcfd3cf597f0123c
fcapristan <fcapristan@gmail.com>
2015-11-02 16:15:15 -0500

    modified test case for new version

--- src/flops_wrapper/test/test_flops_wrapper.py
+++ src/flops_wrapper/test/test_flops_wrapper.py
@@ -7,15 +7,15 @@ import shutil
 
 # Append the path above us, so that we can run the test even if we don't
 # have the package installed.
+
+
+#from flops_wrapper.flops_wrapper import FlopsWrapper
 import sys
 sys.path.append("../")
-#from flops_wrapper.flops_wrapper import FlopsWrapper
 from flops_wrapper import FlopsWrapper
 from openmdao.core.problem import Problem
 from openmdao.core.group import Group
-from compareDump import editFile
-
-
+from openmdao.recorders.dump_recorder import DumpRecorder
 
 class FLOPSWrapperTestCase(unittest.TestCase):
 
@@ -26,11 +26,7 @@ class FLOPSWrapperTestCase(unittest.TestCase):
     def tearDown(self):
         """this teardown function will be called after each test"""
 
-        for filename in ['flops.inp', 'flops.out', 'flops.err', 
-                                    'flops.dump','FPex4',
-                                    'temp_openmado.dump',
-                                    'edited_temp_openmado.dump',
-                                     'edited_old.dump']:
+        for filename in ['flops.inp', 'flops.out', 'flops.err', 'flops.dump', 'FPex4']:
             if os.path.exists(filename):
                 os.remove(filename)
 
@@ -42,37 +38,31 @@ class FLOPSWrapperTestCase(unittest.TestCase):
         os.chdir(dirname)
 
         try:
-            for num in range(1,7):
-    
+            for num in range(1, 7):
                 startfile_name = 'xflp%s.in' % num
                 infile_name = 'xflp%s_openmdao.in' % num
                 outfile_name = 'xflp%s_openmdao.out' % num
                 dumpfile_name = 'xflp%s_openmdao.dump' % num
                 top = Problem()
                 top.root = Group()
-                top.root.add('my_flops',FlopsWrapper())
-
-                #top.root.my_flops.load_model(testFile)    
-                flops_comp = top.root.my_flops
-
-                #flops_comp = FlopsWrapper()
+                top.root.add('flops_comp',FlopsWrapper())
+                flops_comp = top.root.flops_comp    
+         
     
                 # Check input file generation
     
                 flops_comp.load_model(startfile_name)
                 flops_comp.generate_input()
                 top.setup(check=False)
-
                 with open(infile_name, 'r') as inp:
                     result1 = inp.readlines()
                 with open('flops.inp', 'r') as inp:
                     result2 = inp.readlines()
+                
                 lnum = 1
                 for line1, line2 in zip(result1, result2):
-
                     try:
                         self.assertEqual(line1, line2)
-
                     except AssertionError as err:
                         raise AssertionError("line %d doesn't match file %s: %s"
                                              % (lnum, infile_name, err))
@@ -81,35 +71,17 @@ class FLOPSWrapperTestCase(unittest.TestCase):
                 # Check output file parsing
     
                 shutil.copyfile(outfile_name, 'flops.out')
-
                 flops_comp.parse_output()
+    
+                '''with open('flops.dump', 'w') as out:
+                    #dump(flops_comp, stream=out, recurse=True)'''
+                dumpFile = open('flops.dump','w')
+                simple_dump(flops_comp,out_stream=dumpFile)
+                dumpFile.close()
 
-                #generate temporary dump file for OpenMDAO
-                tempdump_name = 'temp_openmado.dump'
-                tempdump= open(tempdump_name,'w')    
-                flops_comp.mydumpVar(out_stream=tempdump)
-                tempdump.close()
-            
-                #edit output files so that they are suitable for comparison
-
-
-
-                #edit old openmdao dump file
-                replace= ['npcon','input.missin.Basic.npcon']
-                ignoreKey=['ERROR','HINT','<flops_wrapper','derivative_exec_count','directory','env_vars',
-                                    'exec_count','force_fd','poll_delay', 'resources', 'return_code', 'timed_out', 'timeout',
-                                    'itername', 'missing_deriv_policy','npcons','nrerun','nseg','<open']
-                editFile(dumpfile_name,'edited_old.dump',ignoreKey,replace)     
-
-                #edit generated openmdao dump file
-                replace=['','']
-                ignoreKey=['thrsop']
-                editFile(tempdump_name,'edited_'+tempdump_name,ignoreKey,replace)     
-
-
-                with open('edited_old.dump', 'r') as inp:
+                with open(dumpfile_name, 'r') as inp:
                     result1 = inp.readlines()
-                with open('edited_'+tempdump_name, 'r') as inp:
+                with open('flops.dump', 'r') as inp:
                     result2 = inp.readlines()
     
                 lnum = 1
@@ -125,6 +97,41 @@ class FLOPSWrapperTestCase(unittest.TestCase):
 
         finally:
             os.chdir(basename)
+
+def simple_dump(comp, out_stream=sys.stdout):
+    # The current dump function in Components only dumps the outputs
+    """
+    Writes a formated dump of this `Component` to file.
+
+    Args
+    ----
+
+    out_stream : an open file, optional
+        Where output is written.  Defaults to sys.stdout.
+
+    """
+
+    ulabel, plabel, uvecname, pvecname = 'u', 'p', 'unknowns', 'params'
+
+    uvec = getattr(comp, uvecname)
+    pvec = getattr(comp, pvecname)
+
+
+    for v in sorted(pvec):
+
+
+        inval = ' \''+v+'\' = '+str(pvec[v])+'\n'
+        out_stream.write( inval)
+
+
+    for v in sorted(uvec):
+        outval =  ' \''+v+'\' = '+str(uvec[v])+'\n'
+        out_stream.write(outval)
+
+
+    out_stream.flush()
+
+
 
 if __name__ == "__main__":
     unittest.main()
